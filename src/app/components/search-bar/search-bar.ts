1832587375ae21ec14c4d0a1782a88dbf11d666d
ale94 <alejandrorua111@gmail.com>
2025-08-16 01:27:03 -0300

    fix: modify components

--- src/app/components/search-bar/search-bar.ts
+++ src/app/components/search-bar/search-bar.ts
@@ -1,10 +1,21 @@
-import { Component } from '@angular/core';
+import { Component, inject, output, signal } from '@angular/core';
+import { MoviesService } from '../../services/movies-service';
+import { Movie } from '../../interfaces/movie.interface';
 
 @Component({
   selector: 'search-bar',
   imports: [],
-  templateUrl: './search-bar.html'
+  templateUrl: './search-bar.html',
 })
 export class SearchBar {
+  movieService = inject(MoviesService);
+  movies = signal<Movie[]>([]);
+  moviesEvent = output<Movie[]>();
 
+  onSearch(query: string) {
+    this.movieService.searchMovies(query).subscribe((resp) => {
+      this.movies.set(resp);
+      this.moviesEvent.emit(this.movies());
+    });
+  }
 }
--- src/app/pages/movie-page/movie-page.ts
+++ src/app/pages/movie-page/movie-page.ts
@@ -1,12 +1,11 @@
 import { Component } from '@angular/core';
 import { Navbar } from '../../components/navbar/navbar';
-import { SearchBar } from '../../components/search-bar/search-bar';
 import { Footer } from '../../components/footer/footer';
 import { MovieList } from '../../components/movie-list/movie-list';
 
 @Component({
   selector: 'movie-page',
-  imports: [Navbar, SearchBar, MovieList, Footer],
+  imports: [Navbar, MovieList, Footer],
   templateUrl: './movie-page.html',
 })
-export class MoviePage {}
+export default class MoviePage {}
