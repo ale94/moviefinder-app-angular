8ca61ae5fc955a2ec9242502946fe4c411718bf1
ale94 <alejandrorua111@gmail.com>
2025-08-16 01:26:30 -0300

    config: config routes

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,17 @@
 import { Routes } from '@angular/router';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  {
+    path: 'movies',
+    loadComponent: () => import('./pages/movie-page/movie-page'),
+  },
+  {
+    path: 'search',
+    loadComponent: () => import('./pages/movie-search/movie-search'),
+  },
+
+  {
+    path: '**',
+    redirectTo: 'movies',
+  },
+];
--- src/app/app.ts
+++ src/app/app.ts
@@ -1,11 +1,11 @@
 import { Component, signal } from '@angular/core';
-import { MoviePage } from './pages/movie-page/movie-page';
+import { RouterOutlet } from '@angular/router';
 
 @Component({
   selector: 'app-root',
-  imports: [MoviePage],
+  imports: [RouterOutlet],
   templateUrl: './app.html',
-  styleUrl: './app.css'
+  styleUrl: './app.css',
 })
 export class App {
   protected readonly title = signal('movie-search-app');
